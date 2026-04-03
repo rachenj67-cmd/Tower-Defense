9bd29047ab38db8423545c8e9be88893c9073507
rachenj67-cmd <rachen.j67@rsu.ac.th>
2026-04-03 21:55:13 +0700

    Add files via upload
    Update Tower Script

---
+++ Script/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+    public int damage = 1;
+
+    void Update()
+    {
+        
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+       
+        Destroy(gameObject, 3f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       
+        if (other.gameObject.name.Contains("Bullet")) return;
+
+        Debug.Log("ชนกับ: " + other.gameObject.name);
+
+        if (other.CompareTag("Enemy"))
+        {
+          
+            EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+
+            if (enemyHealth == null)
+                enemyHealth = other.GetComponentInParent<EnemyHealth>();
+
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage);
+                Debug.Log("สั่งลดเลือดสำเร็จ!");
+            }
+            else
+            {
+                Debug.Log("ERROR: หาไฟล์ EnemyHealth บนตัวซอมบี้ไม่เจอ!");
+            }
+
+            Destroy(gameObject); 
+        }
+    }
+}
